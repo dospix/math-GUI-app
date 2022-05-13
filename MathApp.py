c9e6a5e8a8852c97edc3b5abc7c7d4e7b1069cf3
Daniel <dospinescu_daniel@yahoo.ro>
2022-05-13 18:33:30 +0300

    build python GUI program from .ui file generated by Qt Designer

---
+++ MathApp.py
@@ -0,0 +1,25 @@
+from PyQt6.QtWidgets import QMainWindow, QApplication, QGridLayout, QLabel, QComboBox
+from PyQt6 import uic
+import sys
+
+
+class MathApp(QMainWindow):
+    def __init__(self):
+        super(MathApp, self).__init__()
+
+        uic.loadUi("MathApp.ui", self)
+
+        self.mainWindowGrid = self.findChild(QGridLayout, "gridLayout_3")
+        self.mathAppGrid = self.findChild(QGridLayout, "gridLayout")
+        self.currWindowLabel = self.findChild(QLabel, "currWindowLabel")
+        self.algebraLabel = self.findChild(QLabel, "algebraLabel")
+        self.nameOfCurrWindowLabel = self.findChild(QLabel, "nameOfCurrWindowLabel")
+        self.algebraComboBox = self.findChild(QComboBox, "algebraComboBox")
+
+        self.show()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    UIWindow = MathApp()
+    app.exec()
