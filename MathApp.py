c44f6ae716f676a574a15435c168ac4259949e8f
Daniel <dospinescu_daniel@yahoo.ro>
2022-05-14 11:20:59 +0300

    add labels and combo boxes for different math fields/subfields

--- MathApp.py
+++ MathApp.py
@@ -11,10 +11,17 @@ class MathApp(QMainWindow):
 
         self.mainWindowGrid = self.findChild(QGridLayout, "mainWindowGridLayout")
         self.mathAppGrid = self.findChild(QGridLayout, "gridLayout")
+
         self.currWindowLabel = self.findChild(QLabel, "currWindowLabel")
-        self.algebraLabel = self.findChild(QLabel, "algebraLabel")
         self.nameOfCurrWindowLabel = self.findChild(QLabel, "nameOfCurrWindowLabel")
+        self.algebraLabel = self.findChild(QLabel, "algebraLabel")
         self.algebraComboBox = self.findChild(QComboBox, "algebraComboBox")
+        self.calculusLabel = self.findChild(QLabel, "calculusLabel")
+        self.calculusComboBox = self.findChild(QComboBox, "calculusComboBox")
+        self.probabilityAndCombinatoricsLabel = self.findChild(QLabel, "probabilityAndCombinatoricsLabel")
+        self.probabilityAndCombinatoricsComboBox = self.findChild(QComboBox, "probabilityAndCombinatoricsComboBox")
+        self.trigonometryLabel = self.findChild(QLabel, "trigonometryLabel")
+        self.trigonometryComboBox = self.findChild(QComboBox, "trigonometryComboBox")
 
         self.show()
 
